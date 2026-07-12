1ed32c50e6c6315b327df8bd012500632967f190
Kendrick Lin <kdlin@calpoly.edu>
2026-07-11 23:07:12 -0400

    selecting html with selectElement and then setting textContent.  console logging and window.alert()

---
+++ index.js
@@ -0,0 +1,13 @@
+// interactivity
+// Template literals 
+console.log(`Hello`);
+console.log(`Lelouch is the goat`); 
+
+document.getElementById("myH1").textContent = "Hello"; 
+document.getElementById("myP").textContent = "My favorite artist is 1975"; 
+
+
+let x; 
+x = 100; 
+
+console.log(x);
--- script.js
+++
@@ -1,4 +0,0 @@
-// Bro Code - JavaScript Full Course
-// Code along here. Open index.html, then F12 -> Console to see output.
-
-console.log("Setup works. Start coding.");

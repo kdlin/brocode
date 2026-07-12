2f7181243c9661986486bd2a6398fe36e76dd745
Kendrick Lin <kdlin@calpoly.edu>
2026-07-12 16:46:31 -0400

    refactor: add continue control flow for invalid inputs

---
+++ projects/rps-v2.js
@@ -0,0 +1,58 @@
+// Rock Paper Scissors - combines ch07 If, ch08 Switch/ch09 Ternary logic,
+// ch10 User Input, ch11 game build
+
+let playGame = confirm("Would you like to play Rock Paper Scissors?");
+
+while (playGame) {
+  let rawInput = prompt(`Please select Rock, Paper, or Scissors`);
+
+  if (rawInput) {
+    let playerInput = rawInput.trim().toLowerCase();
+
+    if (
+      playerInput === "rock" ||
+      playerInput === "paper" ||
+      playerInput === "scissors"
+    ) {
+      let compValue = Math.floor(Math.random() * 3);
+      const choices = ["rock", "paper", "scissors"]; 
+      let compChoice = choices[compValue];
+
+      let result =
+        playerInput === "rock"
+          ? compChoice === "rock"
+            ? "tie"
+            : compChoice === "paper"
+              ? "Computer Wins"
+              : "Player Wins"
+          : playerInput === "paper"
+            ? compChoice === "rock"
+              ? "Player Wins"
+              : compChoice === "paper"
+                ? "tie"
+                : "Computer Wins"
+            : compChoice === "rock"
+              ? "Computer Wins"
+              : compChoice === "paper"
+                ? "Player Wins"
+                : "tie";
+
+      alert(
+        `Player Selected: ${playerInput}\nComputer Selected: ${compChoice}\nResult: ${result}`,
+      );
+    } else {
+      alert("Please enter rock, paper, or scissors.");
+      continue; // don't run rest of code
+    }
+  } else {
+    alert("You didn't enter anything - maybe next time.");
+    continue; // retry 
+  }
+  let playAgain = confirm("Would you like to play again?"); 
+  if (playAgain) {
+    continue;
+  } else {
+      break;
+  }
+} 
+

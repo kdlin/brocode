9eb12a098a2c6fedea0388b484ffe5270b1ce4f8
Kendrick Lin <kdlin@calpoly.edu>
2026-07-11 23:29:23 -0400

    Two methods of getting user input. window prompt and creating a form field

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// interactivity
-// Template literals 
-console.log(`Hello`);
-console.log(`Lelouch is the goat`); 
-
-document.getElementById("myH1").textContent = "Hello"; 
-document.getElementById("myP").textContent = "My favorite artist is 1975"; 
 
 
-let x; 
-x = 100; 
 
-console.log(x);
+// let pokemonName = "Pikachu"; 
+// let pokemonAge = 14;
+// let pokemonIsFireType = false; 
+
+
+// document.getElementById("p1").textContent = pokemonName;
+// document.getElementById("p2").textContent = `${pokemonName} is ${pokemonAge} years old`;
+// document.getElementById("p3").textContent = `${pokemonName} is of type Fire: ${pokemonIsFireType}`;
+
+
+let username;
+
+document.getElementById("mySubmit").onclick = function() {
+  username = document.getElementById("myText").value; 
+  document.getElementById("myH1").textContent = `Hello ${username}`;
+}

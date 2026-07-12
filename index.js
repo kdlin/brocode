95dd5e996fe14265c7a65ffd6711a8263a77c190
Kendrick Lin <kdlin@calpoly.edu>
2026-07-11 23:54:26 -0400

    Type Conversions: taking user input as String convert to number and log

--- index.js
+++ index.js
@@ -11,9 +11,27 @@
 // document.getElementById("p3").textContent = `${pokemonName} is of type Fire: ${pokemonIsFireType}`;
 
 
-let username;
+/*  === 2 types of user input window.prompt("input") and create a form 
 
-document.getElementById("mySubmit").onclick = function() {
-  username = document.getElementById("myText").value; 
-  document.getElementById("myH1").textContent = `Hello ${username}`;
-}
+// let username;
+
+// document.getElementById("mySubmit").onclick = function() {
+//   username = document.getElementById("myText").value; 
+//   document.getElementById("myH1").textContent = `Hello ${username}`;
+// } 
+
+
+
+3) type conversions
+// Task: Accept User Input For pokemon currHP as a string then convert to Number
+and subtract 100 from it; 
+*/ 
+
+document.getElementById("submitButton").onclick = function(){
+  let hp = Number(document.getElementById("pokeHp").value); // string user inputted
+  console.log(`Oh no!! Your pokemon got attacked`);
+  let damagedHp = hp - 150;
+  let message = `Your pokemon went from ${hp} hp to ${damagedHp} hp`;
+  console.log(message);
+  console.log(typeof hp, typeof message);
+} 

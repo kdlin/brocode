40ce112b4403f816e692aaa33779c24994890cef
Kendrick Lin <kdlin@calpoly.edu>
2026-07-12 11:27:10 -0400

    Ternary operatros. Next level if else statements. Chain to save lines of code.

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
-// Dealing Random Poker Cards
+// // Dealing Random Poker Cards
 
-const myButton = document.querySelector(".deal-btn");
-const label1= document.querySelector("#label1"); 
-const label2= document.querySelector("#label2"); 
-const label3= document.querySelector("#label3"); 
+// const myButton = document.querySelector(".deal-btn");
+// const label1= document.querySelector("#label1"); 
+// const label2= document.querySelector("#label2"); 
+// const label3= document.querySelector("#label3"); 
 
-const  min = 1; 
-const max = 13; 
-let randomNum1; //exists 
-let randomNum2;
-let randomNum3;
-function randomNumFormula(){return Math.floor(Math.random() * (max - min + 1)) + 1;}
+// const  min = 1; 
+// const max = 13; 
+// let randomNum1; //exists 
+// let randomNum2;
+// let randomNum3;
+// function randomNumFormula(){return Math.floor(Math.random() * (max - min + 1)) + 1;}
 
-myButton.addEventListener('click', () => {
-  randomNum1 = randomNumFormula();
-  label1.textContent = `Card: ${randomNum1}`; 
+// myButton.addEventListener('click', () => {
+//   randomNum1 = randomNumFormula();
+//   label1.textContent = `Card: ${randomNum1}`; 
 
-  randomNum2 = randomNumFormula(); 
-  label2.textContent = `Card: ${randomNum2}`; 
+//   randomNum2 = randomNumFormula(); 
+//   label2.textContent = `Card: ${randomNum2}`; 
 
-  randomNum3 = randomNumFormula();
-  label3.textContent = `Card: ${randomNum3}`;
-})
+//   randomNum3 = randomNumFormula();
+//   label3.textContent = `Card: ${randomNum3}`;
+// })
+
+
+// Code that returns random letter from name 
+const name = "kdlin";
+let randIdx = Math.floor(Math.random() * name.length());
+console.log(name.charAt(randIdx));
+
+
+
+// Conditional: Switch Statements 
+
+// After cases, Switch statements have default case 
+// syntax
+switch("Pikachu") { 
+  case 1:
+    console.log("chuuuu");
+    break;
+  case 2: 
+    console.log("pika"); 
+    break;
+  case "Pikachu": 
+    console.log("Pikachu"); 
+  
+  default:
+    return 0;
+}
+
+// Ternary Operator 
+let grade = 90;
+let letterGrade = 
+  grade > 90 
+  ? "A"
+  : grade > 80 
+  ? "B"
+  : grade > 70
+  ? "C" 
+  : grade > 60
+  ? "D" 
+  : "F"; 
+console.log(letterGrade);
---
+++ notes.md
@@ -0,0 +1,39 @@
+HTML
+- Link stylesheet and index js file 
+- open in live server preview
+- HTML elements should be above the SCRIPT ( In case there are errors) 
+
+Changing text content 
+1) Select via document.getElementById or by className or Name 
+2) .textContent and assign it 
+3) Full -> document.selectById("myP") = "text content" ; 
+
+
+### How to accept User Input
+
+assignment 1) get the h1 and username ( myText input); 
+
+
+
+Type Conversion
+1) get pokemon hp as text then convert into Number and subtract from it 
+
+2) Typecasting strings to Number(); 
+
+Notes
+ - NaN Not a Number 
+ - Number type casting on 
+ - String to Boolean is True ( as long as there is a value );  
+
+DataType conversion for user input is quite important
+
+Boolean for "is user input completed" 
+
+### Const vs Let 
+Assignment to typeError
+
+Accept User Input via text box 
+
+
+### Math object
+- Rounding negatives actually rounds towards 0 

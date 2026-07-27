d2382bf9f1c8a929c131d795d6f886d05a57fdfb
Kendrick Lin <kdlin@calpoly.edu>
2026-07-27 18:37:06 -0400

    add hover eventlistener per card. Add delete button

--- projects/anime_tracker/script.js
+++ projects/anime_tracker/script.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  initApp();
-})
-const initApp =  () => {
   const nav = document.querySelector("nav");
   const adder = document.querySelector("#confirm-add"); 
   const allAnimes = document.querySelector(".all-animes");
+
+  // Create a card to be appended into the all_animes gallery
   const createCard = (nameInput, rating) => { 
     const card = document.createElement("div");
     card.innerHTML = `
-    <div>
       <h3></h3>
       <div class="rating"></div>
-    </div>
+      <button type="button" class="delete-btn">Delete</button>
     `
     card.querySelector("h3").textContent = nameInput;
     card.querySelector(".rating").textContent = `Rating: ${rating}`; 
     card.classList.add("anime-card");
     return card;
   }
+
+  // event listener on Confirm Click
   adder.addEventListener(
     "click",
     (event) => {
@@ -31,4 +30,31 @@ const initApp =  () => {
   false
 );
 
-}
+allAnimes.addEventListener(
+  "click",
+  (event) => { 
+    if (!event.target.matches(".delete-btn")) { return;}
+    const card = event.target.closest(".anime-card"); 
+    if (card) { card.remove();}
+  }
+);
+
+allAnimes.addEventListener(
+  "mouseover",
+  (event) => {
+    const card = event.target.closest(".anime-card");
+    if (card) {
+      card.classList.add("status-watching");
+    }
+  }
+)
+
+allAnimes.addEventListener(
+  "mouseout",
+  (event) => {
+    const card = event.target.closest(".anime-card");
+    if (card) {
+      card.classList.remove("status-watching");
+    }
+  }
+)

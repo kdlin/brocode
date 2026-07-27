cdbb01518b05dd6508892417e38787d96bb43c7a
Kendrick Lin <kdlin@calpoly.edu>
2026-07-27 17:03:40 -0400

    add css for cards and all_anime

--- projects/anime_tracker/script.js
+++ projects/anime_tracker/script.js
@@ -10,18 +10,19 @@ const initApp =  () => {
     card.innerHTML = `
     <div>
       <h3></h3>
-      <div id="rating"></div>
+      <div class="rating"></div>
     </div>
     `
     card.querySelector("h3").textContent = name_input;
     card.querySelector(".rating").textContent = `Rating: ${rating}`; 
+    card.classList.add("anime-card");
     return card;
   }
   adder.addEventListener(
     "click",
     (event) => {
       const name_input = document.querySelector("#name-input");
-      const rating_input = document.querySelector(".rating-input");
+      const rating_input = document.querySelector("#rating-input");
       all_animes.append(create_card(name_input.value, rating_input.value ))
       console.log("anime added");
   },
